d782cbb58243beb89ff3a18c705d2c9d1c7e9402
pablo_kleim <pablo.190777@gmail.com>
2021-10-06 19:15:56 -0300

    ajuste encode fib

--- src/main/java/unisinos/teoriainformacao/compressao/strategy/Fibonacci.java
+++ src/main/java/unisinos/teoriainformacao/compressao/strategy/Fibonacci.java
@@ -3,7 +3,6 @@ package unisinos.teoriainformacao.compressao.strategy;
 import unisinos.teoriainformacao.compressao.file.Message;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,37 +13,31 @@ public class Fibonacci implements Encoder, Decoder {
         return null;
     }
 
-    public String encode(String entrada)
-    {
-        var saida = "";
-        var codeword = "";
+    public String encode(String entrada) {
+        var saida = new StringBuilder();
+        var codeword = new StringBuilder();
 
-        for (char letra: entrada.toCharArray())
-        {
-           var listaFibonacci = calcularFibonacci(letra);
+        for (char letra : entrada.toCharArray()) {
+            var listaFibonacci = calcularFibonacci(letra);
 
-           var somatorio = listaFibonacci.get(0);
-           codeword += '1';
+            var somatorio = listaFibonacci.get(0);
+            codeword.append("1");
 
-           for(var itemFibonacci: listaFibonacci.subList(1, listaFibonacci.size()))
-           {
-               if (somatorio + itemFibonacci <= letra)
-               {
-                   codeword += '1';
-                   somatorio += itemFibonacci;
-               }
-               else
-               {
-                    codeword += '0';
-               }
-           }
+            for (var itemFibonacci : listaFibonacci.subList(1, listaFibonacci.size())) {
+                if (somatorio + itemFibonacci <= letra) {
+                    codeword.append("1");
+                    somatorio += itemFibonacci;
+                } else {
+                    codeword.append("0");
+                }
+            }
 
-           codeword = new StringBuilder(codeword).reverse().toString();
-           saida += codeword + "1";
-           codeword = "";
+            codeword.reverse();
+            saida.append(codeword.append("1"));
+            codeword = new StringBuilder();
         }
 
-        return saida;
+        return saida.toString();
     }
 
     @java.lang.Override
@@ -57,19 +50,17 @@ public class Fibonacci implements Encoder, Decoder {
         return null;
     }
 
-    private static List<Integer> calcularFibonacci(int limiteMaximo)
-    {
+    private static List<Integer> calcularFibonacci(int limiteMaximo) {
         List<Integer> fibonacci = new ArrayList<Integer>();
         fibonacci.add(1);
         fibonacci.add(2);
         fibonacci.add(3);
 
-        int penultimo = fibonacci.get(fibonacci.size() - 1);
+        int penultimo = fibonacci.get(fibonacci.size() - 2);
         int ultimo = fibonacci.get(fibonacci.size() - 1);
 
         int calculado;
-        do
-        {
+        do {
             calculado = penultimo + ultimo;
             fibonacci.add(calculado);
             penultimo = ultimo;
